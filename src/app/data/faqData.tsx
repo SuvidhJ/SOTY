3da44f2be36e5e914941679048fbfb161ddca0e8
Keshav-Aneja <keshav.aneja09@gmail.com>
2024-03-02 23:38:34 +0530

    feat: FAQ's updated

--- src/app/data/faqData.tsx
+++ src/app/data/faqData.tsx
@@ -1,30 +1,80 @@
 export const faqData = [
   {
-    question: "How to Scan?",
+    question: "How do I scan?",
     answer:
-      "Login > Proceed with riddles > Allow Camera to Scan > Select Camera facing back > Start scanning > Submit",
+      "To scan, follow these steps: Login, proceed with the riddles, allow the camera to scan, select the rear-facing camera, start scanning, and finally, submit.",
   },
   {
-    question: "How long will you be logged into the app?",
-    answer: "12 Hours",
+    question: "How long can I stay logged into the app?",
+    answer: "You can stay logged in for up to 12 hours.",
   },
   {
-    question: "Whom to share the login credentials with?",
-    answer: "Only with your teammates.",
+    question: "Whom should I share my login credentials with?",
+    answer: "Share your login credentials only with your teammates.",
   },
   {
-    question: "How many logins at once?",
+    question: "How many logins are allowed simultaneously?",
     answer:
-      "Single user. Multiple users with the same login is not advised at all",
+      "Only single-user logins are recommended; using the same login for multiple users is not advisable.",
   },
   {
-    question: "Max amount of tries before getting a time penalty?",
+    question:
+      "What is the maximum number of attempts before receiving a time penalty?",
     answer:
-      "Maximum of two wrong scans are permitted before getting a time penalty of 2 minutes",
+      "You are allowed a maximum of two incorrect scans before incurring a time penalty of 2 minutes.",
   },
   {
-    question: "What to do if the QR does not scan?",
+    question: "What should I do if the QR code does not scan?",
     answer:
-      "Go to your GoogleLens and decrypt the QR code, inputting the text received manually",
+      "If the QR code fails to scan, use Google Lens to decrypt the code and manually input the received text in the provided text box below the scanner.",
+  },
+  {
+    question: "What should I do if I accidentally get logged out?",
+    answer:
+      "If you happen to get logged out, simply log in again using the same credentials.",
+  },
+  {
+    question: "Can there be multiple riddles for the same location?",
+    answer: "Yes, there can be multiple riddles for the same location.",
+  },
+  {
+    question:
+      "After two wrong submissions if I get to scan again without the timer being over, will it be accepted?",
+    answer:
+      "No, even if the third submission seems to be correct after two wrong attempts, the ban will still be in effect for the allotted 2 minutes.",
+  },
+  {
+    question: "How should I navigate through the website?",
+    answer:
+      "Utilize the buttons within the application to navigate through the website efficiently to get logged in to your account properly.",
+  },
+  {
+    question: "Are Power-ups available during lunch breaks?",
+    answer: "No, powerups are not available during lunch breaks.",
+  },
+  {
+    question: "What is the attacking power of “Riddle Redirect”?",
+    answer:
+      "You can give any of your opponents a riddle to solve, in which they have to reach the nearest game centers to pick their riddles and solve them manually. They have 15 15-minute timers under which they have to take the riddles from the nearest center, solve it, and revert to the nearest helpdesk. If they fail to do so a 50 points reduction will be imposed on the team.Importantly, if you log out during this time, the ban timer will not be counted, and it will persist upon logging back in.",
+  },
+  {
+    question: "What is the attacking power of “Freeze The Breeze”?",
+    answer:
+      "You can freeze any of your opponents for the next 15 minutes under which they won’t be able to solve the riddles for the next 15 minutes. Importantly, if you log out during this time, the ban timer will not be counted, and it will persist upon logging back in.",
+  },
+  {
+    question: "What is the attacking power of “Hundredfold Hit”?",
+    answer:
+      "The “Hundredfold Hit” power allows you to reduce 100 points from another team's total points on the leaderboard.",
+  },
+  {
+    question: "What is the attacking power of “Double-Half Dilemma”?",
+    answer:
+      "When the “Double-Half Dilemma” power is activated, you have two options; first, for the next 15 minutes, a chosen team's points will be added with half the value during this period, and second, for the next 15 minutes, your points will increase at a speed of 2x value during this period.",
+  },
+  {
+    question: "What is the defencing power of all the power-ups?",
+    answer:
+      "If you have the defence of any respective power-ups, then it can be used to protect yourself from the attack of that particular power-up.",
   },
 ];
