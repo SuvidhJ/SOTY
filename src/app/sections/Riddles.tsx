ebc76acdfcf5a0d02b8b068e2fade38f859a4551
Suvidh Jain <139179313+SuvidhJ@users.noreply.github.com>
2025-02-19 00:41:04 +0530

    Update Riddles.tsx

--- src/app/sections/Riddles.tsx
+++ src/app/sections/Riddles.tsx
@@ -38,7 +38,7 @@ const Riddles = ({
     try {
       setLoading(true);
       const response = await axios.get(
-        `https://soty-backend-25.onrender.com/questions/${id}?difficultyLevel=${diff}`,
+        `https://soty-backend-25.onrender.com/questions/${userId}`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
