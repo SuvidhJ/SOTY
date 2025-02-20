c5455f197505c86bd66f2e91fcde13dbefe6b339
Suvidh Jain <139179313+SuvidhJ@users.noreply.github.com>
2025-02-20 23:35:51 +0530

    Update axios.ts

--- src/axios.ts
+++ src/axios.ts
@@ -1,23 +1,29 @@
+// axios.ts
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const BASE_URL = "https://soty-backend-25.vercel.app/"
+const BASE_URL = "https://soty-backend-25.vercel.app/"; 
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
-    withCredentials: true
-})
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+    }
+});
 
-// axiosInstance.interceptors.request.use((config)=>{
-//     // const token = Cookies.get("token");
-//     const token = localStorage.getItem('token');
-//     if (token && token !== ""){
-//         config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-// }, (error)=>{
-//     return Promise.reject(error);
-// })
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default axiosInstance;
-
