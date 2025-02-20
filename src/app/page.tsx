c868cadd9c06544052bda1b3dc397487b360200a
Suvidh Jain <139179313+SuvidhJ@users.noreply.github.com>
2025-02-21 02:56:15 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React, { useState } from "react";
-import Image from "next/image";
+import React, { useState, useEffect } from "react";
 import BaseWrapper from "./wrappers/baseWrapper";
 import NavWrapper from "./wrappers/navWrapper";
 import MainWrapper from "./wrappers/mainWrapper";
@@ -8,44 +7,48 @@ import Navbar from "./sections/Navbar";
 import Landing from "./landing/Landing";
 import Login from "./login/Login";
 import MobileNav from "./sections/MobileNav";
+
 export default function Home() {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [menu, setMenu] = useState<string>("home");
+
+  useEffect(() => {
+    const checkAuth = () => {
+      const token = localStorage.getItem("token");
+      setIsLoggedIn(!!token);
+
+      const savedMenu = localStorage.getItem("menu");
+      if (savedMenu) {
+        setMenu(savedMenu);
+      }
+    };
+
+    checkAuth(); 
+    
+    window.addEventListener("storage", checkAuth);
+
+    return () => {
+      window.removeEventListener("storage", checkAuth);
+    };
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("menu", menu);
+  }, [menu]);
+
   return (
-    <>
-      <BaseWrapper>
-        <NavWrapper>
-          <MobileNav
-            isLoggedIn={isLoggedIn}
-            setMenu={setMenu}
-            menu={menu}
-            setIsLoggedIn={setIsLoggedIn}
-          />
-          <Navbar
-            isLoggedIn={isLoggedIn}
-            setMenu={setMenu}
-            menu={menu}
-            setIsLoggedIn={setIsLoggedIn}
-          />
-        </NavWrapper>
-        <MainWrapper>
-          {isLoggedIn ? (
-            <Landing
-              selectedMenu={menu}
-              setMenu={setMenu}
-              setIsLoggedIn={setIsLoggedIn}
-            />
-          ) : menu === "faq" ? (
-            <Landing
-              selectedMenu={menu}
-              setMenu={setMenu}
-              setIsLoggedIn={setIsLoggedIn}
-            />
-          ) : (
-            <Login setIsLoggedIn={setIsLoggedIn} />
-          )}
-        </MainWrapper>
-      </BaseWrapper>
-    </>
+    <BaseWrapper>
+      <NavWrapper>
+        <MobileNav isLoggedIn={isLoggedIn} setMenu={setMenu} menu={menu} setIsLoggedIn={setIsLoggedIn} />
+        <Navbar isLoggedIn={isLoggedIn} setMenu={setMenu} menu={menu} setIsLoggedIn={setIsLoggedIn} />
+      </NavWrapper>
+      <MainWrapper>
+        {isLoggedIn || menu === "faq" ? (
+          <Landing selectedMenu={menu} setMenu={setMenu} setIsLoggedIn={setIsLoggedIn} />
+        ) : (
+          <Login setIsLoggedIn={setIsLoggedIn} />
+        )}
+      </MainWrapper>
+    </BaseWrapper>
   );
 }
