2babbd5945bc49f12d9ad4b669240df46fb8f5f5
Suvidh Jain <139179313+SuvidhJ@users.noreply.github.com>
2025-02-21 16:21:18 +0530

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -39,7 +39,7 @@ export default function Admin() {
     try {
       const decodedData: CustomJwtPayload = jwtDecode(token);
       if (!decodedData?.isAdmin) {
-        router.push("/");
+        router.push("/admin");
         return;
       }
     } catch (error) {
